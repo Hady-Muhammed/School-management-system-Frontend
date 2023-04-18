db2bb6ed2aebe5922bda6a281849d61d04941991
OmarGamal07 <omar.gamal.dev@gmail.com>
2023-04-18 17:07:50 +0200

    home work

--- src/pages/admin-pages/components/NavbarA.jsx
+++ src/pages/admin-pages/components/NavbarA.jsx
@@ -17,6 +17,7 @@ const NavbarA = () => {
     <nav className="xs:w-[70%] lg:w-full mx-auto flex xs:flex-col lg:flex-row justify-evenly xs:hidden lg:flex py-4 font-mont">
       <Link to={'/a/courses'} className="border-b-4 cursor-pointer border-b-violet-600 p-3 px-6 font-bold hover:scale-[1.1] duration-500 rounded-md">Courses</Link>
       <Link to={'/a/AdminDashboard'} className="border-b-4 cursor-pointer border-b-violet-600 p-3 px-6 font-bold hover:scale-[1.1] duration-500 rounded-md">Examination</Link>
+      <Link to={'/a/schedule'} className="border-b-4 cursor-pointer border-b-violet-600 p-3 px-6 font-bold hover:scale-[1.1] duration-500 rounded-md">Schedule</Link>
       <Link to={'/'} onClick={logOut} className="border-b-4 border-b-violet-600 p-3 px-6 font-bold hover:scale-[1.1] cursor-pointer duration-500 rounded-md">Logout</Link>
     </nav>
 
@@ -26,6 +27,7 @@ const NavbarA = () => {
       <nav className="xs:w-[70%] lg:w-full mx-auto xs:flex lg:hidden xs:flex-col lg:flex-row justify-evenly py-4 font-mont">
         <Link to={'/a/courses'} className="border-b-4 cursor-pointer border-b-violet-600 p-3 px-6 font-bold lk rounded-md">Courses</Link>
         <Link to={'/a/AdminDashboard'} className="border-b-4 cursor-pointer border-b-violet-600 p-3 px-6 font-bold lk rounded-md">Examination</Link>
+        <Link to={'/a/schedule'} className="border-b-4 cursor-pointer border-b-violet-600 p-3 px-6 font-bold lk rounded-md">Schedule</Link>
         
         <Link to={'/'} onClick={logOut} className="border-b-4 border-b-violet-600 p-3 px-6 font-bold lk rounded-md cursor-pointer">Logout</Link>
       </nav>
